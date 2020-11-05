8d1e61782a13fd58231e518066ac54284115a66a
Viktor Senderov <38105253+vsenderov@users.noreply.github.com>
2020-11-05 12:38:42 +0100

    Update init.js

--- js/init.js
+++ js/init.js
@@ -51,7 +51,7 @@
 				medium: {
 					range: '-980',
 					href: '{{ site.baseurl }}/css/style-medium.css',
-					containers: '90%',
+					containers: '50%',
 					grid: {
 						collapse: 1
 					}
